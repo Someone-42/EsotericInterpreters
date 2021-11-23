df629b56733ab80c12108e9da3efb0662521a1b8
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2021-11-23 09:21:43 +0100

    Instruction table

---
+++ Python-Interpreter/ChickenInstructionTable.py
@@ -0,0 +1,20 @@
+class ChickenInstructionInfo:
+    __slots__=("opcode", "name", "chicken_name")
+    def __init__(self, opcode, name, chicken_name) -> None:
+        self.opcode = opcode
+        self.name = name
+        self.chicken_name = chicken_name
+
+CHICKEN_INSTRUCTIONS = [
+    ChickenInstructionInfo(0, "exit", "axe"),
+    ChickenInstructionInfo(1, "chicken", "chicken"),
+    ChickenInstructionInfo(2, "add", "add"),
+    ChickenInstructionInfo(3, "subtract", "fox"),
+    ChickenInstructionInfo(4, "multiply", "rooster"),
+    ChickenInstructionInfo(5, "compare", "compare"),
+    ChickenInstructionInfo(6, "load", "pick"),
+    ChickenInstructionInfo(7, "store", "peck"),
+    ChickenInstructionInfo(8, "jump", "fr"),
+    ChickenInstructionInfo(9, "char", "BBQ"),
+    ChickenInstructionInfo(10, "push", "")
+]
