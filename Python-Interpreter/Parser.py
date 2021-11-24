a0a7eb5dc4600177a0b1d8803837b807373caba4
Someone-42 <tristn.lemarchand@gmail.com>
2021-11-24 17:14:41 +0100

    Parser had errors

--- Python-Interpreter/Parser.py
+++ Python-Interpreter/Parser.py
@@ -1,4 +1,4 @@
-import ChickenInstructionTable as t
+import InstructionInfoTables as t
 
 def chicken_to_minichicken(chicken: str) -> list:
     return [len(line.split(' ')) for line in chicken.split('\n')]
@@ -7,9 +7,9 @@ def minichicken_to_chicken(minichicken: list) -> str:
     return '\n'.join([" ".join(["chicken"] * i) for i in minichicken])
 
 def minichicken_to_instructions(minichicken: list, instruction_infos) -> list:
-    return [(instruction_infos[i].name if i < 10 else (instruction_infos[10].name + ' ' + str(i - 10))) for i in mini_chicken]
+    return [(instruction_infos[i].name if i < 10 else (instruction_infos[10].name + ' ' + str(i - 10))) for i in minichicken]
 
-def instructions_to_minichicken(instructions: list, instrcution_infos) -> str:
+def instructions_to_minichicken(instructions: list, instrcution_infos) -> list:
     d = {}
     for instruction in instrcution_infos:
         d[instruction.name] = instruction.opcode
@@ -26,13 +26,13 @@ if __name__=="__main__":
     try:
         mini_chicken = chicken_to_minichicken(chicken_code)
         assert mini_chicken == [3, 2, 13]
-        print("test 1 passed")
+        print("Test 1 passed")
         assert minichicken_to_chicken(mini_chicken) == chicken_code
-        print("test 2 passed")
+        print("Test 2 passed")
         instrs = minichicken_to_instructions(mini_chicken, t.CHICKEN_INSTRUCTIONS)
         assert instrs == ["subtract", "add", "push 3"]
-        print("test 3 passed")
+        print("Test 3 passed")
         assert instructions_to_minichicken(instrs, t.CHICKEN_INSTRUCTIONS) == mini_chicken
-        print("test 4 passed")
+        print("Test 4 passed")
     except Exception as e:
         print ("/!\ at least one test failed:", e)
