2aeca89a60f460c00a5d622860742bc7638bc1d3
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2021-11-24 10:53:52 +0100

    Chicken instructions changes

--- Python-Interpreter/ChickenInstructionTable.py
+++ Python-Interpreter/ChickenInstructionTable.py
@@ -1,23 +1,35 @@
 class ChickenInstructionInfo:
-    __slots__=("opcode", "name", "chicken_name")
-    def __init__(self, opcode: int, name: str, chicken_name: str) -> None:
+    __slots__=("opcode", "name")
+    def __init__(self, opcode: int, name: str) -> None:
         self.opcode = opcode
         self.name = name
-        self.chicken_name = chicken_name
 
     def __str__(self):
-        return str(self.opcode) + '|' + self.chicken_name
+        return str(self.opcode) + '|' + self.name
 
 CHICKEN_INSTRUCTIONS = [
-    ChickenInstructionInfo(0, "exit", "axe"),
-    ChickenInstructionInfo(1, "chicken", "chicken"),
-    ChickenInstructionInfo(2, "add", "add"),
-    ChickenInstructionInfo(3, "subtract", "fox"),
-    ChickenInstructionInfo(4, "multiply", "rooster"),
-    ChickenInstructionInfo(5, "compare", "compare"),
-    ChickenInstructionInfo(6, "load", "pick"),
-    ChickenInstructionInfo(7, "store", "peck"),
-    ChickenInstructionInfo(8, "jump", "fr"),
-    ChickenInstructionInfo(9, "char", "BBQ"),
-    ChickenInstructionInfo(10, "push", "wing")
+    ChickenInstructionInfo(0, "exit"),
+    ChickenInstructionInfo(1, "chicken"),
+    ChickenInstructionInfo(2, "add"),
+    ChickenInstructionInfo(3, "subtract"),
+    ChickenInstructionInfo(4, "multiply"),
+    ChickenInstructionInfo(5, "compare"),
+    ChickenInstructionInfo(6, "load"),
+    ChickenInstructionInfo(7, "store"),
+    ChickenInstructionInfo(8, "jump"),
+    ChickenInstructionInfo(9, "char"),
+    ChickenInstructionInfo(10, "push")
+]
+CHICKEN_CHICKEN_INSTRUCTIONS = [
+    ChickenInstructionInfo(0, "axe"),
+    ChickenInstructionInfo(1, "chicken"),
+    ChickenInstructionInfo(2, "add"),
+    ChickenInstructionInfo(3, "fox"),
+    ChickenInstructionInfo(4, "rooster"),
+    ChickenInstructionInfo(5, "compare"),
+    ChickenInstructionInfo(6, "pick"),
+    ChickenInstructionInfo(7, "peck"),
+    ChickenInstructionInfo(8, "fr"),
+    ChickenInstructionInfo(9, "BBQ"),
+    ChickenInstructionInfo(10, "wing")
 ]
--- Python-Interpreter/MiniChicken.py
+++ Python-Interpreter/MiniChicken.py
@@ -6,24 +6,33 @@ def chicken_to_minichicken(chicken: str) -> list:
 def minichicken_to_chicken(minichicken: list) -> str:
     return '\n'.join([" ".join(["chicken"] * i) for i in minichicken])
 
-def minichicken_to_instructions(minichicken: list) -> list:
-    return [(t.CHICKEN_INSTRUCTIONS[i].chicken_name if i < 10 else (t.CHICKEN_INSTRUCTIONS[10].chicken_name + ' ' + str(i - 10))) for i in mini_chicken]
+def minichicken_to_instructions(minichicken: list, instruction_infos) -> list:
+    return [(instruction_infos[i].name if i < 10 else (instruction_infos[10].name + ' ' + str(i - 10))) for i in mini_chicken]
 
-def instructions_to_minichicken(minichicken: list) -> str:
-    return '\n'.join([" ".join(["chicken"] * i) for i in minichicken])
+def instructions_to_minichicken(instructions: list, instrcution_infos) -> str:
+    d = {}
+    for instruction in instrcution_infos:
+        d[instruction.name] = instruction.opcode
+    l = []
+    for instruction in instructions:
+        arr = instruction.split(' ')
+        arg = 0 if len(arr) < 2 else int(arr[1])
+        l.append(d[arr[0]] + arg)
+    return l
 
 if __name__=="__main__":
     # Testing stuff
     chicken_code = "chicken chicken chicken\nchicken chicken\nchicken chicken chicken chicken chicken chicken chicken chicken chicken chicken chicken chicken chicken"
-    print(chicken_code)
     try:
         mini_chicken = chicken_to_minichicken(chicken_code)
         assert mini_chicken == [3, 2, 13]
         print("test 1 passed")
         assert minichicken_to_chicken(mini_chicken) == chicken_code
         print("test 2 passed")
-        for c in minichicken_to_instructions(mini_chicken):
-            print(c)
+        instrs = minichicken_to_instructions(mini_chicken, t.CHICKEN_INSTRUCTIONS)
+        assert instrs == ["subtract", "add", "push 3"]
         print("test 3 passed")
-    except:
-        print ("/!\ at least one test failed")
+        assert instructions_to_minichicken(instrs, t.CHICKEN_INSTRUCTIONS) == mini_chicken
+        print("test 4 passed")
+    except Exception as e:
+        print ("/!\ at least one test failed:", e)
