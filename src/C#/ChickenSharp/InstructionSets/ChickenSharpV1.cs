8093307aa2c916812855196438bbd53d66faaed0
Someone-42 <tristn.lemarchand@gmail.com>
2021-12-05 10:03:13 +0100

    Clean up using a try parse for more stability

--- src/C#/ChickenSharp/InstructionSets/ChickenSharpV1.cs
+++ src/C#/ChickenSharp/InstructionSets/ChickenSharpV1.cs
@@ -33,91 +33,74 @@ namespace ChickenSharp.InstructionSets
             }
         );
 
-        internal static void Exit(int? arg, IVM vm)
+        public static void Exit(int? arg, IVM vm)
         {
             vm.Stop();
         }
 
-        internal static void Chicken(int? arg, IVM vm)
+        public static void Chicken(int? arg, IVM vm)
         {
             vm.stack.Push("chicken");
         }
 
-        internal static void Add(int? arg, IVM vm)
+        public static void Add(int? arg, IVM vm)
         {
             object roperand = vm.stack.Pop();
             object loperand = vm.stack.Pop();
-            if (loperand is int li && roperand is int ri)
+            if (TryParseStackValue(loperand, out int li) && TryParseStackValue(roperand, out int ri))
                 vm.stack.Push(li + ri);
             else
                 vm.stack.Push($"{loperand}{roperand}");
         }
 
-        internal static void Subtract(int? arg, IVM vm)
+        public static void Subtract(int? arg, IVM vm)
         {
             object roperand = vm.stack.Pop();
             object loperand = vm.stack.Pop();
-            if (loperand is int li)
-            {
-                if(roperand is int ri)
-                    vm.stack.Push(li - ri);
-                else if (roperand is int[] ria)
-                    vm.stack.Push(li - ria[0] - (ria.Length > 1 ? ria[1] : 0));
-            }
-            else if (loperand is int[] lia)
-            {
-                if (roperand is int ri)
-                    vm.stack.Push(lia[0] + (lia.Length > 1 ? lia[1] : 0) - ri);
-                else if (roperand is int[] ria)
-                    vm.stack.Push(lia[0] - ria[0] - (ria.Length > 1 ? ria[1] : 0) + (lia.Length > 1 ? lia[1] : 0));
-            }
+
+            if (TryParseStackValue(loperand, out int li) && TryParseStackValue(roperand, out int ri))
+                vm.stack.Push(li - ri);
             else
                 throw new ChickenException($"These two types are not subtractable {roperand.GetType()} `{roperand}` and {loperand.GetType()} `{loperand}`");
         }
 
-        internal static void Multiply(int? arg, IVM vm)
+        public static void Multiply(int? arg, IVM vm)
         {
             object roperand = vm.stack.Pop();
             object loperand = vm.stack.Pop();
-            if (loperand is int li)
+            if (TryParseStackValue(loperand, out int li))
             {
-                if (roperand is int[] ari)
-                    vm.stack.Push(li * ari.Sum());
-                else if (roperand is int ri)
+                if (TryParseStackValue(roperand, out int ri))
                     vm.stack.Push(li * ri);
                 else if (roperand is string s)
                     vm.stack.Push(Enumerable.Repeat(s, li));
             }
-            else if (loperand is string s && roperand is int ri)
+            else if (loperand is string s && TryParseStackValue(roperand, out int ri))
                 vm.stack.Push(Enumerable.Repeat(s, ri));
             else
                 throw new ChickenException($"Multiplication is not valid on types : {roperand.GetType()} and {loperand.GetType()}");
         }
 
-        internal static void Compare(int? arg, IVM vm)
+        public static void Compare(int? arg, IVM vm)
         {
             object roperand = vm.stack.Pop();
             object loperand = vm.stack.Pop();
             vm.stack.Push(roperand.ToString() == loperand.ToString());
         }
 
-        internal static void Load(int? arg, IVM vm)
+        public static void Load(int? arg, IVM vm)
         {
             object nextToken = vm.GetNextInstruction();
-            int loadFrom = 0;
-            if (nextToken is int[] a && a[0] == 1) loadFrom = 1;
-            else if (nextToken is int i && i == 1) loadFrom = 1;
+
+            if(!TryParseStackValue(nextToken, out int loadFrom)) throw new ChickenException($"Cannot load from {nextToken} because it is not convertable to int");
 
             try
             {
+                TryParseStackValue(vm.stack.Pop(), out int index);
                 if (loadFrom == 0)
-                {
-                    int index = (int) vm.stack.Pop();
                     vm.stack.Push(vm.stack.GetAt(index > 0 ? index : vm.stack.Length - index)); // User loads from stack, at index pop
-                }
                 else
                 {
-                    int index = (int)vm.stack.Pop();
                     string s = vm.stack.GetAt(0) as string;
                     vm.stack.Push(s[index > 0 ? index : s.Length - index]); // User wants to load from input, gathering the char -> string[index]
                 }
@@ -128,7 +111,7 @@ namespace ChickenSharp.InstructionSets
             }
         }
 
-        internal static void Store(int? arg, IVM vm)
+        public static void Store(int? arg, IVM vm)
         {
             object o = vm.stack.Pop();
             if (o is int index)
@@ -137,32 +120,49 @@ namespace ChickenSharp.InstructionSets
                 throw new ChickenException($"Cannot store value {vm.stack.Pop()} at index {o} because it is not an int");
         }
 
-        internal static void Jump(int? arg, IVM vm)
+        public static void Jump(int? arg, IVM vm)
         {
             object o = vm.stack.Pop();
             if (o is int offset)
             {
                 object condition = vm.stack.Pop();
-                if ((condition is bool b && b) || (condition is int i && i != 0) || (condition is string s && s.Length > 0)) //If the condition is truthy
+                if ((condition is bool b && b) || (TryParseStackValue(condition, out int i) && i != 0) || (condition is string s && s.Length > 0)) //If the condition is truthy
                     vm.instructionPointer += offset;
             }
             else
                 throw new ChickenException($"Cannot Jump using offset {o} because it is not an int");
         }
 
-        internal static void Char(int? arg, IVM vm)
+        public static void Char(int? arg, IVM vm)
         {
             object o = vm.stack.Pop();
-            if (o is int i)
+            if (TryParseStackValue(o, out int i))
                 vm.stack.Push(((char)i).ToString());
             else if (o is string || o is char) vm.stack.Push(o); // Already a string, we dont do anything
             else
                 throw new ChickenException($"Couldn't convert {o} to char");
         }
 
-        internal static void Push(int? arg, IVM vm)
+        public static void Push(int? arg, IVM vm)
         {
             vm.stack.Push(arg ?? 0);
         }
+
+        public static bool TryParseStackValue(object stackValue, out int i)
+        {
+            if (stackValue is int ii)
+            {
+                i = ii;
+                return true;
+            }
+            else if (stackValue is int[] ia)
+            {
+                i = ia.Sum();
+                return true;
+            }
+            i = 0;
+            return false;
+        }
+
     }
 }
