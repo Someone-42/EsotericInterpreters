d0ebec510fe3898c4df1870fd01262564f408b23
Someone-42 <tristn.lemarchand@gmail.com>
2022-03-27 09:42:32 +0200

    Added FixedStack

---
+++ src/C#/ChickenSharp.Tests/FixedStackTests.cs
@@ -0,0 +1,23 @@
+﻿using Esoterics.PspspsInterpreter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.Tests
+{
+    [TestClass]
+    public class FixedStackTests
+    {
+
+        [TestMethod]
+        public void SimpleTest1()
+        {
+            FixedStack<int> stack = new FixedStack<int>(10);
+            stack.Push(42);
+            int value = stack.Pop();
+            Assert.IsTrue(value == 42);
+        }
+
+    }
+}
---
+++ src/C#/ChickenSharp/PspspsInterpreter/FixedStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.PspspsInterpreter
+{
+    public class FixedStack<T> : IEnumerable
+    {
+        private T[] array;
+        private int pointer;
+
+        public readonly int Capacity;
+
+        public FixedStack(int capacity)
+        {
+            array = new T[capacity];
+            pointer = -1;
+            Capacity = capacity;
+        }
+
+        public void Push(T element)
+        {
+            array[++pointer] = element;
+        }
+
+        public T Pop()
+        {
+            return array[pointer--];
+        }
+
+        public int Count()
+        {
+            return pointer + 1;
+        }
+        
+        public bool IsEmpty()
+        {
+            return pointer == -1;
+        }
+
+        public void Clear()
+        {
+            pointer = -1;
+        }
+
+        public T Peek(int index)
+        {
+            if (index > pointer || index < 0)
+                throw new ArgumentOutOfRangeException("index");
+            return array[index];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new FixedStackEnumerator<T>(array);
+        }
+    }
+}
