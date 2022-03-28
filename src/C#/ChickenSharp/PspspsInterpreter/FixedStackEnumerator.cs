4e27dff26188cec677e178115c07d303d785d5ea
Someone-42 <tristn.lemarchand@gmail.com>
2022-03-28 16:41:34 +0200

    FixedStack enumerator object

---
+++ src/C#/ChickenSharp/PspspsInterpreter/FixedStackEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.PspspsInterpreter
+{
+    public class FixedStackEnumerator<T> : IEnumerator
+    {
+
+        private T[] array;
+        private int actualSize;
+        private int pointer = -1;
+
+        public FixedStackEnumerator(T[] array, int pointer)
+        {
+            this.array = array;
+            actualSize = pointer + 1; // The Fixed stack has a pointer defining the position of the last element in the stack -> this position + 1 is the number of elements
+        }
+
+        public T Current => array[pointer];
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            pointer++;
+            return pointer < actualSize;
+        }
+
+        public void Reset()
+        {
+            pointer = -1;
+        }
+    }
+}
