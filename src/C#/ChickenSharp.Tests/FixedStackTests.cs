eabae95e1ab68490999f0cff48ff725ea6226d38
Someone-42 <tristn.lemarchand@gmail.com>
2022-03-30 13:23:02 +0200

    Tests 2

--- src/C#/ChickenSharp.Tests/FixedStackTests.cs
+++ src/C#/ChickenSharp.Tests/FixedStackTests.cs
@@ -19,5 +19,28 @@ namespace Esoterics.Tests
             Assert.IsTrue(value == 42);
         }
 
+        [TestMethod]
+        public void Test2()
+        {
+            FixedStack<int> stack = new FixedStack<int>(10);
+            stack.Push(42);
+            stack.Push(42);
+            stack.Push(69);
+            stack.Push(420);
+            stack.Push(666);
+            stack.Push(13);
+            Assert.AreEqual(stack.Peek(3), 420);
+            try
+            {
+                stack.Peek(42);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is IndexOutOfRangeException);
+            }
+        }
+
+        //TODO: Tests for every function of the stack
+
     }
 }
