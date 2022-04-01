f8cf148184a9957b45107178aada79782d45e4fc
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-01 10:27:55 +0200

    Added PspspsCode object

---
+++ src/C#/ChickenSharp/PspspsInterpreter/PspspsCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.PspspsInterpreter
+{
+    public class PspspsCode
+    {
+
+        public readonly byte[] instructions;
+        public readonly int[] args;
+
+        public PspspsCode(byte[] code, int[] args)
+        {
+            instructions = (byte[]) code.Clone();
+            this.args = (int[]) args.Clone();
+        }
+
+    }
+}
