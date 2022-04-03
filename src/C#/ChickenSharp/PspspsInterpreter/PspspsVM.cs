5ec14a81c6e5744c229107686e4e543573334310
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-03 09:27:23 +0200

    Create PspspsVM.cs

---
+++ src/C#/ChickenSharp/PspspsInterpreter/PspspsVM.cs
@@ -0,0 +1,30 @@
+﻿using Esoterics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.PspspsInterpreter
+{
+    public class PspspsVM
+    {
+
+        public IInstructionSet InstructionSet;
+
+        public FixedStack<int> Memory;
+
+        /// <summary>
+        /// Creates a new VM that can execute Pspsps Code
+        /// </summary>
+        /// <param name="MemSize4">The memory capacity (Stack of Integers, so 4 bytes per added capacity)</param>
+        public PspspsVM(IInstructionSet set, int MemSize4 = 1024)
+        {
+            Memory = new FixedStack<int>(MemSize4);
+        }
+
+        public void Execute(PspspsCode code)
+        {
+
+        }
+
+    }
+}
