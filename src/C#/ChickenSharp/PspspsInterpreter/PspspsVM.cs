79a108688a458dc9482061e0e8a6dca62c279018
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-13 13:27:09 +0200

    Code is in VM from start + Run method implementation

--- src/C#/ChickenSharp/PspspsInterpreter/PspspsVM.cs
+++ src/C#/ChickenSharp/PspspsInterpreter/PspspsVM.cs
@@ -8,28 +8,52 @@ namespace Esoterics.PspspsInterpreter
     public class PspspsVM
     {
 
-        public IInstructionSet InstructionSet;
-
+        public PspspsInstructionSet InstructionSet;
         public FixedStack<int> Memory;
 
         #region Runtime
         public int InstructionPointer;
-        public int[] LabelAddresses;
+        public PspspsCode Code;
         #endregion Runtime
 
         /// <summary>
         /// Creates a new VM that can execute Pspsps Code
         /// </summary>
         /// <param name="MemSize4">The memory capacity (Stack of Integers, so 4 bytes per added capacity)</param>
-        public PspspsVM(IInstructionSet set, int MemSize4 = 1024)
+        public PspspsVM(PspspsInstructionSet set, int MemSize4 = 1024)
         {
             Memory = new FixedStack<int>(MemSize4);
+            InstructionSet = set;
         }
 
         public void Execute(PspspsCode code)
         {
             if (code.InstructionSetKey != InstructionSet.GetKey())
                 throw new Exception($"Incompatible sets. The code is from the {code.InstructionSetKey} instruction set, whilst the VM is running {InstructionSet.GetKey()}");
+
+            Code = code;                // Initializing
+            InstructionPointer = 0;
+
+            while (InstructionPointer < code.Instructions.Length)
+            {
+                //Debug();
+                InstructionSet.Execute(code.Instructions[InstructionPointer], code.Arguments[InstructionPointer], this);
+                InstructionPointer++;
+            }
+
+            InstructionPointer = -1;    // Flushing memory
+            Code = null;
+
+        }
+
+        private void Debug()
+        {
+            int instruction = Code.Instructions[InstructionPointer];
+            int arg = Code.Arguments[InstructionPointer];
+            List<string> memString = new List<string>();
+            foreach (int i in Memory)
+                memString.Add(i.ToString());
+            Console.WriteLine($"STEP IP={InstructionPointer} : {InstructionSet.Instructions[instruction].Name}, {arg} | {string.Join(", ", memString)}");
 
         }
 
