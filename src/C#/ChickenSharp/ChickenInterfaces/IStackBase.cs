8141ed558bfdbd0d41e18714412b284d4d0b16c5
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-28 15:49:21 +0200

    IStack base interface for why i don't remember why i did this

---
+++ src/C#/ChickenSharp/ChickenInterfaces/IStackBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esoterics.Interfaces
+{
+    public interface IStackBase<T>
+    {
+        public T Pop();
+        public void Push(T item);
+        public int Count();
+
+    }
+}
