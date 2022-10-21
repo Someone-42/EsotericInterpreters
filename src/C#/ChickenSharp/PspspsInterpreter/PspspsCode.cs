6186b2db43007a40caf01cecb1f91ac70eeb82bb
Someone-42 <tristn.lemarchand@gmail.com>
2022-10-21 18:28:51 +0200

    Pspsps : Code and VM now check for settings

--- src/C#/ChickenSharp/PspspsInterpreter/PspspsCode.cs
+++ src/C#/ChickenSharp/PspspsInterpreter/PspspsCode.cs
@@ -8,17 +8,22 @@ namespace Esoterics.PspspsInterpreter
     {
 
         public string InstructionSetKey;
+        public string InstructionSetVersion;
+
+        public int ListedMemorySize;
+        public int ListedFunctionStackSize;
 
         public readonly byte[] Instructions;
         public readonly int[] Arguments;
         public readonly int[] LabelAddresses;
 
-        public PspspsCode(byte[] code, int[] args, int[] labelAddresses, string instrKey)
+        public PspspsCode(byte[] code, int[] args, int[] labelAddresses, string instrKey, string instrSetVersion)
         {
             Instructions = (byte[]) code.Clone();
             Arguments = (int[]) args.Clone();
             LabelAddresses = (int[]) labelAddresses.Clone();
             InstructionSetKey = instrKey;
+            InstructionSetVersion = instrSetVersion;
         }
 
     }
